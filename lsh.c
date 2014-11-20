e4517fcda48816ed72efaec6ca5ffb5e3fb5a584
Johan Bregell <johan_bregell@hotmail.com>
2014-11-20 01:21:27 +0100

    Fixed change directory

--- lsh.c
+++ lsh.c
@@ -34,7 +34,7 @@
 void PrintCommand(int, Command *);
 void PrintPgm(Pgm *);
 void stripwhite(char *);
-int runCommand(int n,Pgm *p);
+int runCommand(int n, Pgm *p);
 /* When non-zero, this global means the user is done using this program. */
 int done = 0;
 
@@ -44,52 +44,50 @@ int done = 0;
  * Description: Gets the ball rolling...
  *
  */
-int main(void)
-{
-  Command cmd;
-  int n;
+int main(void) {
+    Command cmd;
+    int n;
 
-  while (!done) {
+    while (!done) {
 
-    char *line;
-    line = readline("> ");
+        char *line;
+        line = readline("> ");
 
-    if (!line) {
-      /* Encountered EOF at top level */
-      done = 1;
+        if (!line) {
+            /* Encountered EOF at top level */
+            done = 1;
+        } else {
+            /*
+             * Remove leading and trailing whitespace from the line
+             * Then, if there is anything left, add it to the history list
+             * and execute it.
+             */
+            stripwhite(line);
+
+            if (*line) {
+                add_history(line);
+                /* execute it */
+                n = parse(line, &cmd);
+                PrintCommand(n, &cmd);
+            }
+        }
+
+        /* Own stuff testing */
+
+
+        /*
+         * Traverse through linked list, and fork process child per program
+         * Sets up pipeline between child processes.
+         */
+        runCommand(0, cmd.pgm);
+
+
+        /* Own stuff testing - END*/
+        if (line) {
+            free(line);
+        }
     }
-    else {
-      /*
-       * Remove leading and trailing whitespace from the line
-       * Then, if there is anything left, add it to the history list
-       * and execute it.
-       */
-      stripwhite(line);
-
-      if(*line) {
-        add_history(line);
-        /* execute it */
-        n = parse(line, &cmd);
-        PrintCommand(n, &cmd);
-      }
-    }
-    
-    /* Own stuff testing */
-    
-
-    /*
-     * Traverse through linked list, and fork process child per program
-     * Sets up pipeline between child processes.
-     */
-    runCommand(0,cmd.pgm);
-
-
-    /* Own stuff testing - END*/
-    if(line) {
-      free(line);
-    }
-  }
-  return 0;
+    return 0;
 }
 
 /*
@@ -97,82 +95,96 @@ int main(void)
  * Sets up pipeline between child processes.
  * Recursive function
  */
-int runCommand(int n,Pgm *p){
-	pid_t pid;
-	if (p == NULL) {
-		return -1;
-	}
-	else {
-		/* Create pipe */
-		int pipefd[2];
+int runCommand(int n, Pgm *p) {
+    pid_t pid;
+    if (p == NULL) {
+        return -1;
+    } else {
+        /* Create pipe */
+        int pipefd[2];
 
-		if (n>0){
-			if (pipe(pipefd) == 0) {
-				fprintf(stderr,"Parent %i: Created pipe\n",n);
-			}
-		}
+        if (n > 0) {
+            if (pipe(pipefd) == 0) {
+                fprintf(stderr, "Parent %i: Created pipe\n", n);
+            }
+        }
 
-		/* fork a child process */
-		pid = fork();
-		if (pid < 0){
-			fprintf(stderr, "Parent %i: Fork failed",n);
-			return 1;
-		}
+        /* fork a child process */
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "Parent %i: Fork failed", n);
+            return 1;
+        } else if (pid > 0) {
+            /* Parent process */
 
-		else if (pid >0) {
-		/* Parent process */
+            /*** SET UP READING END ***/
+            /* Skip pipe for first process */
+            if (n > 0) {
 
-			/*** SET UP READING END ***/
-			/* Skip pipe for first process */
-			if (n>0) {
-
-				/* close writing end */
-				if (close(pipefd[1]) != 0){
-					fprintf(stderr,"Closing pipefd[1] failed\n");
-				}
-				/* Connect reading end to STDIN */
-				dup2(pipefd[0],0);
-				/* close reading end */
-				if (close(pipefd[0]) != 0){
-					fprintf(stderr,"Closing pipefd[1] failed\n");
-				}
-			}
+                /* close writing end */
+                if (close(pipefd[1]) != 0) {
+                    fprintf(stderr, "Closing pipefd[1] failed\n");
+                }
+                /* Connect reading end to STDIN */
+                dup2(pipefd[0], 0);
+                /* close reading end */
+                if (close(pipefd[0]) != 0) {
+                    fprintf(stderr, "Closing pipefd[1] failed\n");
+                }
+            }
 
 
-			wait(NULL);
-			fprintf(stderr,"Parent %i: Finished waiting\n",n);
-		}
+            wait(NULL);
+            fprintf(stderr, "Parent %i: Finished waiting\n", n);
+        } else {
+            /* child process */
+            fprintf(stderr, "Hej from Child%i\n", n);
 
-		else {
-		/* child process */
-			fprintf(stderr,"Hej from Child%i\n",n);
+            /*** SET UP WRITING END ***/
+            /* Skip pipe for first process */
+            if (n > 0) {
+                /* close reading end */
+                if (close(pipefd[0]) != 0) {
+                    fprintf(stderr, "Closing pipefd[1] failed\n");
+                }
+                /* Connect writing end to STDOUT */
+                dup2(pipefd[1], 1);
 
-			/*** SET UP WRITING END ***/
-			/* Skip pipe for first process */
-			if (n>0) {
-				/* close reading end */
-				if (close(pipefd[0]) != 0){
-					fprintf(stderr,"Closing pipefd[1] failed\n");
-				}
-				/* Connect writing end to STDOUT */
-				dup2(pipefd[1],1);
-
-				/* close writing end */
-				if (close(pipefd[1]) != 0){
-					fprintf(stderr,"Closing pipefd[1] failed\n");
-				}
-			}
-			/* Recursive call to runCommand, itself */
-			runCommand(n+1,p->next);
-			/* Execute program binary */
-			execvp(p->pgmlist[0],p->pgmlist);
-		}
-		return 0;
-	}
+                /* close writing end */
+                if (close(pipefd[1]) != 0) {
+                    fprintf(stderr, "Closing pipefd[1] failed\n");
+                }
+            }
+            /* Recursive call to runCommand, itself */
+            runCommand(n + 1, p->next);
+            /* Execute program binary */
+            if (strcmp(p->pgmlist[0], "cd") == 0) {
+                int size = 256;
+                int length = 0;
+                char path[256];
+                if (strcmp(p->pgmlist[1], "..") == 0) {
+                    /*Get Current path and put it in path*/
+                    getcwd(path, size);
+                    /*Print the path*/
+                    printf("%s\n", path);
+                    /*Get the length of the path string*/
+                    length = strlen(path);
+                    /*search backwards for for first occurrence of / and replace with \0 */
+                    while (path[length] != '/')
+                        length--;
+                    path[length] = '\0';
+                    printf("%s\n", path);
+                } else {
+                    strcpy(path, p->pgmlist[1]);
+                }
+                chdir(path);
+            } else {
+                execvp(p->pgmlist[0], p->pgmlist);
+            }
+        }
+        return 0;
+    }
 }
-
-
-
 
 /*
  * Name: PrintCommand
@@ -181,13 +193,12 @@ int runCommand(int n,Pgm *p){
  *
  */
 void
-PrintCommand (int n, Command *cmd)
-{
-  printf("Parse returned %d:\n", n);
-  printf("   stdin : %s\n", cmd->rstdin  ? cmd->rstdin  : "<none>" );
-  printf("   stdout: %s\n", cmd->rstdout ? cmd->rstdout : "<none>" );
-  printf("   bg    : %s\n", cmd->bakground ? "yes" : "no");
-  PrintPgm(cmd->pgm);
+PrintCommand(int n, Command *cmd) {
+    printf("Parse returned %d:\n", n);
+    printf("   stdin : %s\n", cmd->rstdin ? cmd->rstdin : "<none>");
+    printf("   stdout: %s\n", cmd->rstdout ? cmd->rstdout : "<none>");
+    printf("   bg    : %s\n", cmd->bakground ? "yes" : "no");
+    PrintPgm(cmd->pgm);
 }
 
 /*
@@ -197,24 +208,22 @@ PrintCommand (int n, Command *cmd)
  *
  */
 void
-PrintPgm (Pgm *p)
-{
-  if (p == NULL) {
-    return;
-  }
-  else {
-    char **pl = p->pgmlist;
+PrintPgm(Pgm *p) {
+    if (p == NULL) {
+        return;
+    } else {
+        char **pl = p->pgmlist;
 
-    /* The list is in reversed order so print
-     * it reversed to get right
-     */
-    PrintPgm(p->next);
-    printf("    [");
-    while (*pl) {
-      printf("%s ", *pl++);
+        /* The list is in reversed order so print
+         * it reversed to get right
+         */
+        PrintPgm(p->next);
+        printf("    [");
+        while (*pl) {
+            printf("%s ", *pl++);
+        }
+        printf("]\n");
     }
-    printf("]\n");
-  }
 }
 
 /*
@@ -223,22 +232,21 @@ PrintPgm (Pgm *p)
  * Description: Strip whitespace from the start and end of STRING.
  */
 void
-stripwhite (char *string)
-{
-  register int i = 0;
+stripwhite(char *string) {
+    register int i = 0;
 
-  while (whitespace( string[i] )) {
-    i++;
-  }
-  
-  if (i) {
-    strcpy (string, string + i);
-  }
+    while (whitespace(string[i])) {
+        i++;
+    }
 
-  i = strlen( string ) - 1;
-  while (i> 0 && whitespace (string[i])) {
-    i--;
-  }
+    if (i) {
+        strcpy(string, string + i);
+    }
 
-  string [++i] = '\0';
+    i = strlen(string) - 1;
+    while (i > 0 && whitespace(string[i])) {
+        i--;
+    }
+
+    string [++i] = '\0';
 }
